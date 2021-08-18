cc93163f3393dc9ae947b8993d42b8f1dc4fad13
DianaMutekhele <89154488+DianaMutekhele@users.noreply.github.com>
2021-08-18 20:13:12 +0300

    Add files via upload

---
+++ Diana R script.R
@@ -0,0 +1,38 @@
+########################################
+#####Assignment#####
+#1. Comment to explain your code.
+
+#this is addition
+25+27
+
+#this is subtraction
+89-62
+
+#2.Carry out some calculation
+my_addition<- 23+56+35
+my_addition
+
+my_multiplication<- 45*56
+my_multiplication
+
+my_subraction<-100-24
+my_subraction
+
+#3.Some evidence of data importation
+
+
+CMS_Users_csv<- read.csv('CMS_Users.csv')
+View(CMS_Users_csv)
+
+CMS_Users_csv<- write.csv('CMS_Users.csv')
+write.csv('CMS_Users.csv')
+
+#Create and use a simple R function
+
+x=c(25,54,86,79,89)
+mean(x)
+
+x=4
+sqrt(x)
+
+#5. Preferably compress as a zip file and submit the zip file
